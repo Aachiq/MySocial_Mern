b67d44c6f684c7db2f3070779a1005b2d49cc7e5
El_aachiq <hocine.aachiq@gmail.com>
2023-07-22 23:38:34 +0100

    setup SocialAPp back uisng typescript

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express,{Application,Request,Response} from 'express';
+import dotenv from "dotenv";
+
+const app: Application = express();
+dotenv.config();
+
+app.get('/',(req: Request,res: Response) => {
+    res.end("hello node Typescript & ORM sequlize cli");
+})
+
+app.listen(process.env.PORT,()=>console.log(`app is runnng at 'http://localhost:${process.env.PORT}'`));
