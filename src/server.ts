bd0a11d5f7ecd34850788cad787f4f6d1b5affcb
El_aachiq <hocine.aachiq@gmail.com>
2023-07-22 23:44:06 +0100

    fix chnages

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app: Application = express();
 dotenv.config();
 
 app.get('/',(req: Request,res: Response) => {
-    res.end("hello node Typescript & ORM sequlize cli");
+    res.end("hello node Typescript & ORM sequlize cli migrations");
 })
 
 app.listen(process.env.PORT,()=>console.log(`app is runnng at 'http://localhost:${process.env.PORT}'`));
